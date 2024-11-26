0047cb6af32fcab9b012cf3cb8107b91e2e02eee
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-11-26 15:48:21 +0530

    Add POST method to process receipt. Test with Postman

--- src/main/java/com/kalpit00/Fetch_Rewards/controller/ReceiptController.java
+++ src/main/java/com/kalpit00/Fetch_Rewards/controller/ReceiptController.java
@@ -1,12 +1,30 @@
 package com.kalpit00.Fetch_Rewards.controller;
 
+import com.kalpit00.Fetch_Rewards.entity.Receipt;
 import com.kalpit00.Fetch_Rewards.service.ReceiptService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequestMapping("/receipts")
+
 public class ReceiptController {
 
     @Autowired
     private ReceiptService receiptService;
+
+    @PostMapping("/process")
+    public ResponseEntity<Receipt> process(@RequestBody Receipt receipt) {
+        log.info("Process request received: " + receipt);
+        Long id = receiptService.processReceipt(receipt);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
 }
--- src/main/java/com/kalpit00/Fetch_Rewards/service/ReceiptService.java
+++ src/main/java/com/kalpit00/Fetch_Rewards/service/ReceiptService.java
@@ -1,12 +1,20 @@
 package com.kalpit00.Fetch_Rewards.service;
 
 import com.kalpit00.Fetch_Rewards.Repository.ReceiptRepository;
+import com.kalpit00.Fetch_Rewards.entity.Receipt;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class ReceiptService {
 
     @Autowired
     private ReceiptRepository receiptRepository;
+
+    public Long processReceipt(Receipt receipt) {
+        receiptRepository.save(receipt);
+        return receipt.getReceiptId();
+    }
 }
