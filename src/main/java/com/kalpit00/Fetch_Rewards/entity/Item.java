18234472d45a89c9aa3d4372ccea8cba3e317f3c
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-11-26 15:36:33 +0530

    Add data sanitation using jakarta.validation.constraints

--- src/main/java/com/kalpit00/Fetch_Rewards/entity/Item.java
+++ src/main/java/com/kalpit00/Fetch_Rewards/entity/Item.java
@@ -1,8 +1,13 @@
 package com.kalpit00.Fetch_Rewards.entity;
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
+
 
 // for now, use a normal embeddable POJO class.
 // later can add primary id to uniquely identify each item across multiple receipts
@@ -11,6 +16,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Embeddable
 public class Item {
+
+    @NotNull
+    @NotBlank
     private String shortDescription;
+
+    @NotNull
+    @Min(0)
+    @Max(Long.MAX_VALUE)
     private Double price;
 }
--- src/main/java/com/kalpit00/Fetch_Rewards/entity/Receipt.java
+++ src/main/java/com/kalpit00/Fetch_Rewards/entity/Receipt.java
@@ -1,6 +1,8 @@
 package com.kalpit00.Fetch_Rewards.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +11,9 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 
 @Entity
 @Data
@@ -20,14 +25,22 @@ public class Receipt {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long receiptId; // Use this to uniquely identify each receipt
 
+    @NotBlank
     private String retailerName;
+    @NotNull
     private Double total;
 
     // use Embeddable for Items to create a column in receipt for each item in the collection
+    @NotNull
     @ElementCollection
     private List<Item> items;
 
     // Can also use Strings for date and time. Add data sanitation later
+    @NotNull
+    @JsonFormat(pattern="yyyy-MM-dd")
     private LocalDate purchaseDate;
+
+    @NotNull
+    @JsonFormat(pattern = "HH:mm")
     private LocalTime purchaseTime;
 }
