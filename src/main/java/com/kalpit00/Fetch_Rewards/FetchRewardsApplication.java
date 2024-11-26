551e39bfaf47f686e5143aea51040ff776d06a80
kalpit00 <kalpitpatel@Kalpits-MacBook-Air.local>
2024-11-26 14:10:06 +0530

    Initial commit using Spring Initializer

---
+++ src/main/java/com/kalpit00/Fetch_Rewards/FetchRewardsApplication.java
@@ -0,0 +1,13 @@
+package com.kalpit00.Fetch_Rewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchRewardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchRewardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalpit00/Fetch_Rewards/FetchRewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalpit00.Fetch_Rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchRewardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
